a66d7ef7625a8aef9dfa1249d006aa793341ba22
yunting <yunting@localhost.localdomain>
2016-06-19 20:03:40 +0800

    test1

---
+++ 1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html: charset=utf-8">
+</head>
+<body>
+<p>
+<?php
+$my_book = "我的小写的book";
+$my_book = "我的大写的book";
+
+echo "<br/>";
+echo $my_BOOK;
+echo "<br/>";
+
+$my_book = "这次奇迹要发生了";
+echo $my_book;
+echo "<br/>";
+?>
+</p>
+</body>
+</html>
+
